962ef3a0d5fd6afd2df12ac3b09fd18ddf9f3357
Youcef <34604972+youcefs21@users.noreply.github.com>
2020-03-10 11:46:38 -0500

    as
    asdasdadasd

--- 2020InfiniteRecharge/src/main/java/frc/robot/subsystems/Chassis.java
+++ 2020InfiniteRecharge/src/main/java/frc/robot/subsystems/Chassis.java
@@ -7,66 +7,27 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.PIDSubsystem;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
-
-public class Chassis extends SubsystemBase {
+public class Chassis extends PIDSubsystem {
   /**
    * Creates a new Chassis.
    */
-
-  TalonSRX right, left;
-  VictorSPX rightSlave, leftSlave;
-  
-
   public Chassis() {
-    //init speed controllers
-    right = new TalonSRX(Constants.rightTalon);
-    left = new TalonSRX(Constants.leftTalon);
-    rightSlave = new VictorSPX(Constants.rightSlave);
-    leftSlave = new VictorSPX(Constants.leftSlave);
-
-    //set brakemode
-    right.setNeutralMode(NeutralMode.Brake);
-    left.setNeutralMode(NeutralMode.Brake);
-    rightSlave.setNeutralMode(NeutralMode.Brake);
-    leftSlave.setNeutralMode(NeutralMode.Brake);
-
-    //set slaves
-    rightSlave.follow(right);
-    leftSlave.follow(left);
-    
-    
+    super(
+        // The PIDController used by the subsystem
+        new PIDController(0, 0, 0));
   }
 
   @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-
+  public void useOutput(double output, double setpoint) {
+    // Use the output here
   }
 
-  public void tankDrive(double left, double right){
-    this.left.set(ControlMode.PercentOutput, left);
-    this.right.set(ControlMode.PercentOutput, right);
-    
+  @Override
+  public double getMeasurement() {
+    // Return the process variable measurement here
+    return 0;
   }
-
-  public void stop(){
-    left.set(ControlMode.PercentOutput, 0);
-    right.set(ControlMode.PercentOutput, 0);
-  }
-
-  public int getleftVelocity(){
-    return this.left.getActiveTrajectoryVelocity();
-  }
-  public int getrightVelocity(){
-    return this.right.getActiveTrajectoryVelocity();
-  }
-
 }
